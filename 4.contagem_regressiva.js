8b13345f68abb29988a70021bfeaed3a71851dc7
Júlio Neto <nettojulio@hotmail.com>
2021-06-08 21:09:20 -0300

    Deu pouco trabalho mas não subestimei.

---
+++ 4.contagem_regressiva.js
@@ -0,0 +1,11 @@
+function solucao(numero) {
+
+    let sequencia = []
+    let seguinte = ""
+
+    for (let i = 0; i <= numero; i++) {
+        seguinte = numero - i
+        console.log(seguinte)
+    } console.log("KABUM")
+
+}
